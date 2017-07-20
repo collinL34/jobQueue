dbe4b9673af38873b931012b3239a7dd54fbdd2a
collinl34 <collinlshelby@gmail.com>
2017-07-19 18:00:20 -0700

    :sparkles: req.body error fix when creating job, test with postman successful

--- app/controllers/jobsController.js
+++ app/controllers/jobsController.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let app = express();
-let router = express.Router();
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,28 +13,15 @@ app.get('/', function(req, res) {
     });
 });
 
-// app.post('/', function(req, res) {
-//     Job.create({
-//         url: req.body.url,
-//         siteData: 'blank'
-//     }, function(err, job) {
-//         if (err)
-//             return res.send(err);
-//         res.send(job);
-//     });
-// });
-
-router.post('/', function(req, res) {
-
+app.post('/', function(req, res) {
     Job.create({
-            url: req.body.url,
-            siteData: 'blank'
-        },
-        function(err, job) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(job);
-        });
-
+        url: req.query.url,
+        siteData: 'blank'
+    }, function(err, job) {
+        if (err)
+            return res.send(err);
+        res.send(job);
+    });
 });
 
 module.exports = app;
